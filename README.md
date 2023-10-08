de3a88da16401ebc55d807082f65182fd0bda83f
hoangnv735 <deserteaglexiii@gmail.com>
2023-10-08 13:54:55 +0700

    add features

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 An extension to quickly peek a Medium article when you are blocked from medium.com
+
+# Change log 
+
+## 0.1.0
+
+- Load text only cache
+- Open Firefox Reader View (depends on sites, mannually open Reader View if the page does not automatically change)
+- Add keyboard shortcut (default: `Alt + A`)
--- background.js
+++ background.js
@@ -1,11 +1,25 @@
-// Listen for clicks on the browser action icon
-browser.browserAction.onClicked.addListener(function (tab) {
-  // Get the current tab's URL
-  browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const currentURL = tabs[0].url;
-    const cacheURL = `http://webcache.googleusercontent.com/search?q=cache:${currentURL}`;
+const cake = () => {
+  const onUpdateListener = (tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+      browser.tabs.onUpdated.removeListener(onUpdateListener);
+      browser.tabs.toggleReaderMode(tabId);
+    }
+  }
+  browser.tabs.onUpdated.addListener(onUpdateListener);
+  browser.tabs.query({ active: true, currentWindow: true }).then(tabs => tabs[0]).then((tab) => {
+    // console.log(tab);
+    var cacheURL = `http://webcache.googleusercontent.com/search?q=cache:${tab.url}&strip=1`;
+    // cacheURL = `about:reader?url=${encodeURIComponent(cacheURL)}`;
     browser.tabs.update({ url: cacheURL });
-    console.log('Hello');
   });
+};
+browser.browserAction.onClicked.addListener(cake);
+
+// Add Keyboard shortcuts
+browser.commands.onCommand.addListener((command) => {
+    switch (command) {
+        case 'cake':
+            cake();
+    }
 });
 
